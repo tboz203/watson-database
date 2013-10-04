6df2c4f73efa15abfc12f6f61004ce5ecaddd665
tboz203 <tboz203@gmail.com>
2013-10-04 08:28:58 -0500

    Beginning to add Angular bindings

--- js/DatabaseApp.js
+++ js/DatabaseApp.js
@@ -3,24 +3,42 @@ angular.module('DatabaseApp', ['ui.bootstrap']);
 var DatabaseController = function($scope){
     $scope.message = "This is a test";
 
-    $scope.oneAtATime = true;
+    $scope.relations = [
+        {link: "rel1", name: "Relation 1"},
+        {link: "rel2", name: "Relation 2"},
+        {link: "rel3", name: "Relation 3"}
+    ]
 
-    $scope.groups = [
-        {
-        title: "Dynamic Group Header - 1",
-        content: "Dynamic Group Body - 1"
-        },
-        {
-        title: "Dynamic Group Header - 2",
-        content: "Dynamic Group Body - 2"
-        }
-    ];
+    $scope.getRelColumns = function($scope){
+        return [
+            {link: "val1", name: "Value 1"},
+            {link: "val2", name: "Value 2"},
+            {link: "val3", name: "Value 3"}
+        ];
+    }
 
-    $scope.items = ['Item 1', 'Item 2', 'Item 3'];
+    $scope.getRelConditions = function($scope){
+        return [
+            {link: "lt", name: "less than"},
+            {link: "gt", name: "greater than"},
+            {link: "eq", name: "equal to"}
+        ];
+    }
 
-    $scope.addItem = function() {
-        var newItemNo = $scope.items.length + 1;
-        $scope.items.push('Item ' + newItemNo);
-    };
-
+    $scope.getComparisonValues = function($scope){
+        return [
+            {link: "val1", name: "Value 1"},
+            {link: "val2", name: "Value 2"},
+            {link: "val3", name: "Value 3"}
+        ];
+    }
+    
+    $scope.table = {name: "Test",
+        rows: [
+            [1, 2, 3, 4, 5],
+            [6, 7, 8, 9, 0],
+            ['a', 'b', 'c', 'd', 'e'],
+            ['f', 'g', 'h', 'i', 'j']
+        ]
+    }
 }
